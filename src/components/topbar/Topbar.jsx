81544059775390a3a748cd25a38f82a636b8c6a0
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-22 23:18:21 +0500

    done navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './topbar.scss'
+import { Person, Mail } from '@mui/icons-material';
 
-export default function topbar() {
+export default function topbar( { menuOpen, setMenuOpen } ) {
   return (
-    <div className='topbar'>
-      
+    <div className={"topbar " + (menuOpen && "active")}>
+        <div className='wrapper'>
+            <div className="left">
+
+                <a href='#intro' className="logo">genius.</a>
+
+                <div className="itemContainer">
+                    <Person className="icon" />
+                    <span>+44 924 12 56</span>
+                </div>
+
+                <div className="itemContainer">
+                    <Mail className="icon" />
+                    <span>sachin@gmail.com</span>
+                </div>
+
+            </div>
+            <div className="right">
+                <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)} >
+                  <span className="line1"></span>
+                  <span className="line2"></span>
+                  <span className="line3"></span>
+                </div>
+            </div>
+        </div>      
     </div>
-  )
+  );
 }

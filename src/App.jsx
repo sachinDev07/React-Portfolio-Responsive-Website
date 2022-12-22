513c15bc92bd650c33a708588af9516a7e07295f
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-22 23:16:13 +0500

    done till menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import Portfolio from './components/portfolio/Portfolio';
 import Work from './components/work/Work';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
+import Menu from './components/menu/Menu';
 import './app.scss'
+import { useState } from 'react';
 
 function App() {
+
+  const [menuOpen,setMenuOpen] = useState(true)
+
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro/>
         <Portfolio/>

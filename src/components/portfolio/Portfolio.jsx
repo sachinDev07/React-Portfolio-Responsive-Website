00c1eef88ca53f780162dd95a5d326d1bc797356
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-24 23:57:19 +0500

    Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,84 @@
-import React from 'react'
+import { ListItem } from '@mui/material';
+import React, { useState } from 'react'
+import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss'
 
 export default function Portfolio() {
+
+  const [selected, setSelected] = useState("featured")
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "desing",
+      title: "Desing",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
   return (
     <div className="portfolio" id="portfolio">
-  
+        <h1>Portfolio</h1>
+        <ul>
+          {
+            list.map((item)=>(
+              <PortfolioList
+               title={item.title}
+               active={selected === item.id}
+               setSelected={setSelected}
+               id={item.id}
+             />
+            ))
+          }
+        </ul>
+        <div className="container">
+        <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://cdn.sanity.io/images/ordgikwe/production/109f6d58b9590d4f8b2cc9a94aa97331e8d08087-1600x1200.jpg?w=1600&h=1200&auto=format" alt="" />
+            <h3>Banking App</h3>
+          </div>
+        </div>
     </div>
   )
 }

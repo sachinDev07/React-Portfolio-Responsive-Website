891c8f2435f861b9d08165d1386485cec49db904
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-26 00:47:30 +0500

    done work section

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './work.scss'
 
 export default function Work() {
+
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const data = [
+    {
+      id: "1",
+      icon: "./images/mobile.png",
+      title: "Web Design",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./images/globe.png",
+      title: "Mobile Application",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./images/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+
+  const handClick = (way) => {
+      way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0 )
+  };
+
   return (
     <div className="works" id="works">
-      
+      <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }} >
+          {
+            data.map((d)=>(
+          <div className="container">
+              <div className="item">
+                  <div className="left">
+                      <div className="leftContainer">
+                          <div className="imageContainer">
+                            <img src={d.icon} alt="" />
+                          </div>
+                          <h2>{d.title}</h2>
+                            <p>
+                              {d.desc}
+                            </p>
+                            <span>Projects</span>
+                      </div>
+                  </div>
+                  <div className="right">
+                    <img src={d.img} alt="" />
+                  </div>
+              </div>
+          </div>
+            ))
+          }
+      </div>
+      <img src="images/arrow.png" className='arrow left' onClick={()=>handClick("left")} alt="" />
+      <img src="images/arrow.png" className='arrow right' onClick={()=>handClick()} alt="" />
     </div>
   )
 }
